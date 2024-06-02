84cf1cd3119d9764dd3ac2d1a92a36e31e4b4af8
Grant Zou <grantbtzou@gmail.com>
2024-06-02 03:20:35 -0400

    replaced apostrophe

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
       <p className="mt-4">The Contemplative Cavalier is an undergraduate philosophy journal run by students at the University of Virginia</p>
       <p className="max-w-5xl text-xs mb-8 absolute bottom-0">Although this organization has members who are University of Virginia students and may have University employees associated or engaged in its activities and affairs, 
           the organization is not a part of or an agency of the University. It is a separate and independent organization which is responsible for and manages its own activities and affairs. 
-          The University does not direct, supervise or control the organization and is not responsible for the organization's contracts, acts or omissions.</p>   
+          The University does not direct, supervise or control the organization and is not responsible for the organization&apos;s contracts, acts or omissions.</p>   
       
     </div>
     
