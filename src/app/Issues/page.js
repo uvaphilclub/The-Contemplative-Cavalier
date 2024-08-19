fc0d06151ffe93882cd63fbb24066cebeab8c25c
Grant Zou <grantbtzou@gmail.com>
2024-08-18 22:09:25 -0400

    added icon/cover

--- src/app/Issues/page.js
+++ src/app/Issues/page.js
@@ -19,6 +19,9 @@ const res = await fetch('https://us-east-1-shared-usea1-02.cdn.hygraph.com/conte
                 document {
                     url
                 }
+                cover {
+                    url
+                }
                 subBoardAcceptanceRate
                 totalAcceptanceRate
                 percentOfPapersReturned
@@ -43,21 +46,6 @@ export default async function Page(){
         <div className="ml-10p sm:ml-15p" id="Issues">
             <div>
                 <h1 className="text-4xl sm:text-6xl">Issues</h1>
-                <div className="py-8">
-                    <h1 className="text-4xl">First Issue</h1>
-                    <div className="flex flex-wrap justify-center sm:justify-normal gap-x-36 gap-y-8 mr-10p sm:mr-0 mt-8 mb-8">
-                        <div className="flex items-center sm:ml-24 h-96 w-64 bg-gray-100">
-                            <p className="text-3xl text-center">This is the hard coded example</p>
-                        </div>
-                        <div className="grid content-center text-xl">
-                            <p>Number of submissions: </p>
-                            <p>Submission board acceptance rate: </p>
-                            <p>Editorial board acceptance rate: </p>
-                            <p>Total acceptance rate: </p>
-                            <p>Percentage of papers returned for revisions: </p>
-                        </div>
-                    </div>
-                </div>
                 {issues.map((issue,index) => (
                 <Issue key={index} issue={issue}/>
                 ))}
--- src/app/components/issue.js
+++ src/app/components/issue.js
@@ -4,8 +4,8 @@ export default function Issue({issue}){
         <h1 className="text-4xl">{issue.title}</h1>
         <div className="flex flex-wrap justify-center sm:justify-normal gap-x-36 gap-y-8 mr-10p sm:mr-0 mt-8 mb-8">
             <a href={issue.document.url}>
-                <div className="flex items-center sm:ml-24 h-96 w-64 bg-gray-100">
-                    <p className="text-3xl text-center">Clickable preview of pdf</p>
+                <div className="flex items-center sm:ml-24 h-96 w-64">
+                    <img src={issue.cover.url}></img>
                 </div>
             </a>
             <div className="grid content-center text-xl">
